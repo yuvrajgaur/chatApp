510a3498bc8be410f39a4fa63e38e5b1f83e6c01
Yuvraj Singh Gaur <yuvrajsinghgaur12@gmail.com>
2026-07-17 00:36:45 +0530

    changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
-import path from "path";
-
 import { connectDB } from "./lib/db.js";
 
 import userRouter from "./routes/user.route.js";
@@ -13,15 +10,17 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
-const __dirname = path.resolve();  
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      process.env.CLIENT_URL
+    ],
     credentials: true,
   })
 );
@@ -29,13 +28,6 @@ app.use(
 app.use("/api/user", userRouter);
 app.use("/api/messages", messageRouter);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
